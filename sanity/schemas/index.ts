5862dba11c17acb295305dc6c2047a95b015af3b
Batsinda Keynes benoit <batsindakeynesbenoit@Batsindas-MacBook-Air.local>
2026-03-27 12:48:31 +0200

    remove

--- sanity/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import { programSchema } from '@/sanity/schemas/program';
-import { siteSettingsSchema } from '@/sanity/schemas/site-settings';
-import { testimonialSchema } from '@/sanity/schemas/testimonial';
-
-export const schemaTypes = [programSchema, testimonialSchema, siteSettingsSchema];
